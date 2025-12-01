264833b5f3453af6656a40fec1b44a870cc2d42f
naturel1 <moi@arnaudhubert.be>
2025-12-01 14:31:19 +0100

    day_1

---
+++ day_1/first_star.py
@@ -0,0 +1,35 @@
+file_path: str = "./puzzle.txt"
+
+
+def import_puzzle() -> list[str]:
+    with open(file_path, "r") as file:
+        puzzle = [line.strip() for line in file]
+        return puzzle
+
+
+def main():
+    dial: int = 50
+    result: int = 0
+    puzzle: list[str] = import_puzzle()
+    print(f"Puzzle: {puzzle}")
+    for move in puzzle:
+        if move.startswith("R"):
+            for _ in range(int(move[1:])):
+                if dial == 99:
+                    dial = 0
+                else:
+                    dial += 1
+        elif move.startswith("L"):
+            for _ in range(int(move[1:])):
+                if dial == 0:
+                    dial = 99
+                else:
+                    dial -= 1
+        if dial == 0:
+            result += 1
+        print(f"Moved {move} and dial now at {dial}")
+    print(f"Final dial position: {dial}")
+    print(f"Number of positions marked: {result}")
+
+if __name__ == "__main__":
+    main()
